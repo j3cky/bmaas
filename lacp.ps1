40028daee7a3f1b20665e95c069a1e91a6e91ae0
j3cky <67102006+j3cky@users.noreply.github.com>
2022-06-09 16:22:54 +0700

    Create lacp.ps1

---
+++ lacp.ps1
@@ -0,0 +1,27 @@
+#---------------UpdateDVSLacpGroupConfig_Task---------------
+
+$lacpGroupSpec = New-Object VMware.Vim.VMwareDvsLacpGroupSpec[] (1)
+
+$lacpGroupSpec[0] = New-Object VMware.Vim.VMwareDvsLacpGroupSpec
+
+$lacpGroupSpec[0].LacpGroupConfig = New-Object VMware.Vim.VMwareDvsLacpGroupConfig
+
+$lacpGroupSpec[0].LacpGroupConfig.Mode = 'passive'
+
+$lacpGroupSpec[0].LacpGroupConfig.Ipfix = New-Object VMware.Vim.VMwareDvsLagIpfixConfig
+
+$lacpGroupSpec[0].LacpGroupConfig.LoadbalanceAlgorithm = 'srcDestIpTcpUdpPortVlan'
+
+$lacpGroupSpec[0].LacpGroupConfig.Vlan = New-Object VMware.Vim.VMwareDvsLagVlanConfig
+
+$lacpGroupSpec[0].LacpGroupConfig.Name = 'lag1'
+$lacpGroupSpec[0].LacpGroupConfig.UplinkNum = 2
+
+$lacpGroupSpec[0].Operation = 'add'
+
+$_this = Get-View -Id 'VmwareDistributedVirtualSwitch-dvs-1194'
+
+$_this.UpdateDVSLacpGroupConfig_Task($lacpGroupSpec)
+
+
+#----------------- End of code capture -----------------
